33a7bdacdd78809a1cdbe9e40aefc7a12459377f
Arief Maffrudin <arief_maffrudin@linkaja.id>
2022-05-31 07:33:52 +0700

    Secure token
    add token

--- core/src/main/java/com/github/ariefannur/modular/core/infra/HeaderInterceptor.kt
+++ core/src/main/java/com/github/ariefannur/modular/core/infra/HeaderInterceptor.kt
@@ -1,12 +1,13 @@
 package com.github.ariefannur.modular.core.infra
 
+import com.github.ariefannur.modular.core.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class HeaderInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "token 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")
+            .addHeader("Authorization", "token ${BuildConfig.TOKEN}")
             .build()
         return chain.proceed(request)
     }
